e372fa758fcc29fc9db21376bceb7c63a7426f4a
James Coleman <jtc331@gmail.com>
2019-02-23 11:50:12 -0500

    Add gitconfig and tmux.conf.

--- setup-all.sh
+++ setup-all.sh
@@ -2,4 +2,5 @@
 
 ./setup-packages.sh
 ./setup-repos.sh
+./setup-dotfiles.sh
 ./setup-vim.sh
---
+++ setup-dotfiles.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -sf $PWD/tmux.conf $HOME/.tmux.conf
+ln -sf $PWD/gitconfig $HOME/.gitconfig
