e03d508e3d58f6e0eaf8f9d410a6b5af5440e440
James Coleman <jtc331@gmail.com>
2019-02-23 21:12:35 -0500

    Add vim-dotfiles setup.

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [[ ! -d "$HOME/.vim" ]]; then
+  git clone git@github.com:braintreeps/vim_dotfiles "$HOME/.vim"
+  ~/.vim/activate.sh
+else
+  echo "Already installed vim_dotfiles; if you want to update please run:"
+  echo "  pushd ~/.vim; git pull; ./activate.sh; popd"
+fi
+
 ln -sf $PWD/bash_profile $HOME/.bash_profile
 ln -sf $PWD/tmux.conf $HOME/.tmux.conf
 ln -sf $PWD/gitconfig $HOME/.gitconfig
