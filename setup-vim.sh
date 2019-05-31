cb48dde2742444999bf467fccf9d2e0e588ccd41
jcoleman <james.coleman@getbraintree.com>
2019-05-31 19:37:42 +0000

    Fix vim setup to use proper repo directory.
    Previously you had to execute the script from the repo directory otherwise
    it woud create broken symlinks.

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-ln -sf $PWD/vimrc.bundles.local $HOME/.vimrc.bundles.local
-ln -sf $PWD/vimrc_local $HOME/.vimrc_local
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+ln -sf "$DIR/vimrc.bundles.local" "$HOME/.vimrc.bundles.local"
+ln -sf "$DIR/vimrc_local" "$HOME/.vimrc_local"
 vim +PlugInstall +PlugUpdate +PlugClean +qall
