aa1de47e11bb43a95d0104b4b69e71a0f328721c
jcoleman <james.coleman@getbraintree.com>
2019-06-22 18:43:02 +0000

    Preconfigure PG test pane with PG<arg> ENV vars.

--- setup-tmux.sh
+++ setup-tmux.sh
@@ -10,6 +10,9 @@ tmux split-window -v
 tmux select-pane -t 0
 tmux send-keys -t 0 "cd $CLONE_DIRECTORY/postgres; vim" C-m
 tmux send-keys -t 1 "cd $CLONE_DIRECTORY/postgres" C-m
+tmux send-keys -t 1 "export PATH=$CLONE_DIRECTORY/postgresql-test/bin:\$PATH" C-m
+tmux send-keys -t 1 "export PGHOST=localhost" C-m
+tmux send-keys -t 1 "export PGPORT=5444" C-m
 
 tmux new-window -n "test-postgres"
 tmux split-window -h
