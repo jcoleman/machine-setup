c571a3d66d009eef9f4ef2ce1fe279ca078625c6
jcoleman <james.coleman@getbraintree.com>
2019-07-01 20:37:44 +0000

    Fix PG<ENV> settings to go into proper tmux pane.

--- setup-tmux.sh
+++ setup-tmux.sh
@@ -10,12 +10,12 @@ tmux split-window -v
 tmux select-pane -t 0
 tmux send-keys -t 0 "cd $CLONE_DIRECTORY/postgres; vim" C-m
 tmux send-keys -t 1 "cd $CLONE_DIRECTORY/postgres" C-m
-tmux send-keys -t 1 "export PATH=$CLONE_DIRECTORY/postgresql-test/bin:\$PATH" C-m
-tmux send-keys -t 1 "export PGHOST=localhost" C-m
-tmux send-keys -t 1 "export PGPORT=5444" C-m
 
 tmux new-window -n "test-postgres"
 tmux split-window -h
 tmux select-pane -t 0
 tmux send-keys -t 0 "cd $CLONE_DIRECTORY/postgres" C-m
+tmux send-keys -t 0 "export PATH=$CLONE_DIRECTORY/postgresql-test/bin:\$PATH" C-m
+tmux send-keys -t 0 "export PGHOST=localhost" C-m
+tmux send-keys -t 0 "export PGPORT=5444" C-m
 tmux send-keys -t 1 "cd $CLONE_DIRECTORY/postgres-dev-tools" C-m
