f8e1a5059db8a809196488dff65dd17768369f15
James Coleman <jtc331@gmail.com>
2020-03-09 17:39:46 -0400

    Standardize on apt-get not apt.

--- setup-packages.sh
+++ setup-packages.sh
@@ -87,7 +87,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     # Install Yarn/Node.
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-    sudo apt update && sudo apt install yarn
+    sudo apt-get update && sudo apt-get install yarn
 
     # Install Docker.
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
