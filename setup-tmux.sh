9850b039ca76511f218495ce4b39d2f069d39bd8
James Coleman <jtc331@gmail.com>
2020-03-28 09:34:55 -0400

    Fixup path export in postgres testing pane.

--- setup-tmux.sh
+++ setup-tmux.sh
@@ -15,7 +15,7 @@ tmux new-window -n "test-postgres"
 tmux split-window -h
 tmux select-pane -t 0
 tmux send-keys -t 0 "cd $CLONE_DIRECTORY/postgres" C-m
-tmux send-keys -t 0 "export PATH=$CLONE_DIRECTORY/postgresql-test/bin:\$PATH" C-m
+tmux send-keys -t 0 "export PATH=\$HOME/postgresql-test/bin:\$PATH" C-m
 tmux send-keys -t 0 "export PGHOST=localhost" C-m
 tmux send-keys -t 0 "export PGPORT=5444" C-m
 tmux send-keys -t 1 "cd $CLONE_DIRECTORY/postgres-dev-tools" C-m
