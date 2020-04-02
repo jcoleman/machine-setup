9bc5244fdbb61ab345db92098b25846b8a1fadd7
James Coleman <james.coleman@getbraintree.com>
2020-04-02 15:37:42 +0000

    Add -y to apt-get install yarn.

--- setup-packages.sh
+++ setup-packages.sh
@@ -115,7 +115,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     # Install Yarn/Node.
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-    sudo apt-get update && sudo apt-get install yarn
+    sudo apt-get update && sudo apt-get install -y yarn
 
     # Install Docker.
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
