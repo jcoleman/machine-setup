99077085045b9e9b7c0b3a8fac38889458ea79f6
jcoleman <jtc331@gmail.com>
2022-02-17 17:02:31 -0500

    Case OS conf for Linux

--- configure-os-conf.sh
+++ configure-os-conf.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# Allow gdb to attach to other user processes.
-# I find this particularly useful for PostgreSQL development so that I can
-# (without sudo) attach gdb to the backend process running as the postgres user.
-sudo sed -i "s/kernel.yama.ptrace_scope = 1/kernel.yama.ptrace_scope = 0/" /etc/sysctl.d/10-ptrace.conf
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  # Allow gdb to attach to other user processes.
+  # I find this particularly useful for PostgreSQL development so that I can
+  # (without sudo) attach gdb to the backend process running as the postgres user.
+  sudo sed -i "s/kernel.yama.ptrace_scope = 1/kernel.yama.ptrace_scope = 0/" /etc/sysctl.d/10-ptrace.conf
+fi
