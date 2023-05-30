5b2bb816b84b66eae61a6d7278518cb6194e516b
jcoleman <jtc331@gmail.com>
2023-05-30 08:56:20 -0400

    Fix homebrew installation

--- setup-packages.sh
+++ setup-packages.sh
@@ -183,7 +183,7 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
   # Install homebrew for package management.
   ! BREW_INSTALLED=$(which brew)
   if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/jamecoleman/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
   fi
